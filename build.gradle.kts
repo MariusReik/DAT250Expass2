a544ec24a9c80ace2d61d1b63f231761092a980f
MariusReik <marius.b.reikeraas@gmail.com>
2025-09-17 13:30:36 +0200

    Implemented JPA entities and passing PollsTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,27 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.5"
-	id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "com.example"
+group = "no.hvl.dat250"
 version = "0.0.1-SNAPSHOT"
-description = "Demo project for Spring Boot"
+description = "DAT250 Expass4 JPA Experiment"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+	toolchain { languageVersion = JavaLanguageVersion.of(21) }
 }
 
-repositories {
-	mavenCentral()
-}
+repositories { mavenCentral() }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.hibernate.orm:hibernate-core:7.1.1.Final")
+	implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
+	implementation("com.h2database:h2:2.3.232")
+
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
